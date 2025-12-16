be934d0abc62c72232e62b112902755ff4a9de3d
PHChiang333 <frank79530@gmail.com>
2025-12-17 01:40:19 +0800

    feat: Add controller for product CRUD and search with sp

--- AuditLogAPI/Model/Dto/ProductDto.cs
+++ AuditLogAPI/Model/Dto/ProductDto.cs
@@ -7,4 +7,20 @@
         public decimal Price { get; set; }
         public DateTime CreatedAt { get; set; }
     }
+
+    public class ProductPutDto
+    {
+        //public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        //public DateTime CreatedAt { get; set; }
+    }
+
+    public class ProductPostDto
+    {
+        //public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        //public DateTime CreatedAt { get; set; }
+    }
 }
